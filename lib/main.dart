317924703da58e33e15a33566ff9d495783b7d5a
Haaziq386 <haaziqjamal@gmail.com>
2024-12-12 13:24:16 +0400

    App completed with launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class BookDiscoveryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Book Discovery',
       theme: ThemeData(
         primarySwatch: Colors.blue,
